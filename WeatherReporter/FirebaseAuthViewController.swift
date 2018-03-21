6231be18e12a9eac2dd9a99bb05fddd72daec77c
(s) Toby Bessant <tbessant@pm7-2002-mac.local>
2018-03-21 12:03:38 +0000

    firebase preparation

---
+++ WeatherReporter/FirebaseAuthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FirebaseAuthViewController.swift
+//  WeatherReporter
+//
+//  Created by (s) Toby Bessant on 21/03/2018.
+//  Copyright © 2018 (s) Toby Bessant. All rights reserved.
+//
+
+import UIKit
+
+class FirebaseAuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
