b3315d3c6b32a576ffaa9d03ce38cff2de0fcd08
(s) Toby Bessant <tbessant@pm7-2002-mac.local>
2018-04-18 16:33:22 +0100

    Added error messages upon invalid data entry or database failure.

--- WeatherReporter/AddReportViewController.swift
+++ WeatherReporter/AddReportViewController.swift
@@ -47,8 +47,30 @@ class AddReportViewController: UIViewController {
         if let tempString = TemperatureTextInput.text, let temp = Int(tempString),
             let desc = DescriptionTextInput.text
         {
-            dh.sendData(temp: temp, desc: desc)
+            if dh.sendData(temp: temp, desc: desc) {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                alertView("ERROR", "Database submission failed.")
+            }
+        } else {
+            alertView("ERROR", "Please enter valid data.")
         }
+        
     }
-}
+    
+    func alertView(_ title: String, _ msg: String) {
+        
+        let dialog = UIAlertController(title: title,
+                                       message: msg,
+                                       preferredStyle: UIAlertControllerStyle.alert)
+        
+        let okButtonOnAlertAction = UIAlertAction(title: "Dismiss", style: .default)
+        { (action) -> Void in
+            dialog.dismiss(animated: true, completion: nil)
+        }
+        
+        dialog.addAction(okButtonOnAlertAction)
+        
+        self.present(dialog, animated: false, completion: nil)
+    }}
 
--- WeatherReporter/DataHandler.swift
+++ WeatherReporter/DataHandler.swift
@@ -8,35 +8,49 @@
 
 import Foundation
 import Firebase
+import UIKit
 
 public class DataHandler
 {
     let db = Firestore.firestore()
-
     
-    func sendData(temp: Int, desc: String)
+    func sendData(temp: Int, desc: String) //change to bool for validation
     {
         var ref: DocumentReference? = nil
-        ref = db.collection("test").addDocument(data:
-            [
+        ref = db.collection("WeatherSubmissions").addDocument(data:[
                 "Date": generateDate(),
+                "Time": generateTime(),
                 "Description": desc,
                 "Temperature": temp,
                 ]) { err in
                     if let err = err {
                         print("Error adding document: \(err)")
+                        
                     } else {
                         print("Document added with ID: \(ref!.documentID)")
+                        
                     }
         }
     }
+    
     func generateDate() -> String
     {
         let date = Date()
         let df = DateFormatter()
-        df.dateFormat = "dd.MM.yyyy"
+        df.dateStyle = .short
+
+        return df.string(from: date)
+    }
+    
+    func generateTime() -> String
+    {
+        let date = Date()
+        let df = DateFormatter()
+        df.dateStyle = .none
+        df.timeStyle = .short
         
         return df.string(from: date)
     }
     
+   
 }
